b1f50001b1bfd82fd989545608279856fa03f57e
Swed0ua <shmagala2001@gmail.com>
2025-12-01 14:35:50 +0200

    fix(AI Асистент Callsapp read status)

--- connect_to_gapi.py
+++ connect_to_gapi.py
@@ -129,12 +129,14 @@ def get_new_messages(service, query=''):
             print("sender-",sender.strip())
             print("subject-",subject.strip())
 
-            if "АІ Асистент Callsapp" in subject and "analytics@jotlink.net" in sender.strip(): 
+            if "АІ Асистент Callsapp" in subject.strip() and "analytics@jotlink.net" in sender.strip(): 
                 try:
+                    print("read [AI Асистент Callsapp]")
                     [html_body, isHtml] = get_html_body_with_mail(msg)
                     if not html_body : continue
                     html_body = html_body.replace("<br>", "\n").replace("&nbsp;", " ")
                     loop.run_until_complete(send_message_to_group_ai_telephonia(html_body))
+                    set_read_status(service, message)
                 except Exception as e:
                     print('read [AI Асистент Callsapp] Error', e)
 
